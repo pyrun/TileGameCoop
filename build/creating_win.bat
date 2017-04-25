e70efde89724b8e5aba03cd65780a4f3ba0cd271
pyrun <thenightalex@googlemail.com>
2017-04-25 12:12:29 +0200

    tiled start point for the level editor

--- build/creating_win.bat
+++ build/creating_win.bat
@@ -7,20 +7,23 @@ echo   [1] Windows32 Debug
 echo   [2] Windows32 Release
 echo   [3] Alle Obj-dateien löschen
 echo   [4] Erstelle makefile
-echo   [5] CMD
-echo   [6] Exit
+echo   [5] TileGameCoop build
+echo   [6] CMD
+echo   [7] Exit
 echo.
 
 set asw=0
 set /p asw="Bitte Auswahl eingeben: "
 
 SET folder="win\%DATE%\"
+SET folderTiled="win\tiled\"
 
 if %asw%==1 goto WIND
 if %asw%==2 goto WINR
 if %asw%==4 goto MAKEFILE
 if %asw%==3 goto OBJDELETING
-if %asw%==5 goto CMD
+if %asw%==5 goto TILED_BUILD
+if %asw%==6 goto CMD
 goto THEEND
 
 :WIND
@@ -39,6 +42,11 @@ goto END
 cbp2make.exe -in ..\src\TileGameCoop.cbp -out ..\src\makefile
 goto END
 
+:TILED_BUILD
+del %folderTiled% /s /q
+xcopy "..\bin\WinDebug32" %folderTiled% /I
+xcopy "..\res" %folderTiled% /E /C /Q /I /Y
+goto END
 
 :CMD
 cd ..\src\
